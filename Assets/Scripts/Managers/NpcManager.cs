72db7bc659c6df9922a04e1a41cb6f37b04029c7
gregf <115318076+gregf@users.noreply.github.com>
2026-06-18 18:50:52 +0300

    more on the previus with the npc script

--- Assets/Scripts/Managers/NpcManager.cs
+++ Assets/Scripts/Managers/NpcManager.cs
@@ -11,6 +11,13 @@ public class Npc : MonoBehaviour
     private bool PlayerIsNear = false;
     [SerializeField] private GameObject IconSprite;
 
+    [Header("Config")]
+    [SerializeField] private bool startPoint = true;
+    [SerializeField] private bool finishPoint = true;
+
+    private string questId;
+    private QuestState currentQuestState;
+
 
     private void OnEnable()
     {
@@ -38,6 +45,27 @@ public class Npc : MonoBehaviour
         {
             EventsManager.Instance.dialogueEvents.EnterDialogue(dialogueKnotName);
             IconSprite.SetActive(false);
+        }
+        else 
+        {
+            // start or finish a quest
+            if (currentQuestState.Equals(QuestState.CAN_START) && startPoint)
+            {
+                EventsManager.Instance.questEvents.StartQuest(questId);
+            }
+            else if (currentQuestState.Equals(QuestState.CAN_FINISH) && finishPoint)
+            {
+                EventsManager.Instance.questEvents.FinishQuest(questId);
+            }
+        }
+    }
+
+    private void QuestStateChange(Quest quest)
+    {
+        // only update the quest state if this point has the corresponding quest
+        if (quest.info.id.Equals(questId))
+        {
+            currentQuestState = quest.state;
         }
     }
     
